72d61be5c335027d70f6fc547a999265fad36423
Xiaojun Luo <xiaojun.luo@thoughtworks.com>
2020-05-09 21:24:29 +0800

    [Xiaojun && Yangliu] add test `should_return_ticket_when_store_package_given_locker_robot_is_available`

---
+++ src/test/java/cn/xpbootcamp/locker_robot/LockerRobotTest.java
@@ -0,0 +1,16 @@
+package cn.xpbootcamp.locker_robot;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import sun.security.krb5.internal.Ticket;
+
+public class LockerRobotTest {
+
+    @Test
+    void should_return_ticket_when_store_package_given_locker_robot_is_available() {
+        LockerRobot lockerRobot = new LockerRobot(10);
+        Bag bag = new Bag("my bag");
+        Ticket ticket = lockerRobot.store(bag);
+        Assertions.assertNotNull(ticket);
+    }
+}
