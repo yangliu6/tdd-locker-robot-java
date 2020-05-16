f6434c4ecd45b5c04c170c9ae570bf5df23b1333
Xiaojun Luo <xiaojun.luo@thoughtworks.com>
2020-05-16 20:55:21 +0800

    [Xiaojun && Yangliu] add test `should_store_into_the_first_available_locker_and_return_ticket_when_store_package_given_robot_have_many_available_lockers`

--- src/test/java/cn/xpbootcamp/locker_robot/RobotTest.java
+++ src/test/java/cn/xpbootcamp/locker_robot/RobotTest.java
@@ -18,4 +18,21 @@ class RobotTest {
 
         Assertions.assertNotNull(ticket);
     }
+
+    @Test
+    void should_store_into_the_first_available_locker_and_return_ticket_when_store_package_given_robot_have_many_available_lockers() {
+        Robot robot = new Robot();
+        Locker theFirstLocker = new Locker(0);
+        Locker theSecondLocker = new Locker(1);
+        Locker theThirdLocker = new Locker(1);
+        robot.add(theFirstLocker);
+        robot.add(theSecondLocker);
+        robot.add(theThirdLocker);
+
+        Bag bag = new Bag();
+        Ticket ticket = robot.store(bag);
+
+        Assertions.assertEquals(theSecondLocker, robot.getLockerWithTicket(ticket));
+        Assertions.assertNotNull(ticket);
+    }
 }
