f41c18be9056f2a4838936830d721d2c190b40d4
Xiaojun Luo <xiaojun.luo@thoughtworks.com>
2020-05-16 20:46:59 +0800

    [Xiaojun && Yangliu] fix test

--- src/main/java/cn/xpbootcamp/locker_robot/Robot.java
+++ src/main/java/cn/xpbootcamp/locker_robot/Robot.java
@@ -1,5 +1,20 @@
 package cn.xpbootcamp.locker_robot;
 
-public class Robot {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Robot {
+    List<Locker> lockers;
+
+    public Robot() {
+        lockers = new ArrayList<>();
+    }
+
+    public void add(Locker locker) {
+        lockers.add(locker);
+    }
+
+    public Ticket store(Bag bag) {
+        return new Ticket();
+    }
 }
