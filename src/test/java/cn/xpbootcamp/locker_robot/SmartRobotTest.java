118295ca75ebbb01473c7a7de317ff614c2c3db9
yangliu <1695938413@qq.com>
2020-05-22 00:32:21 +0800

    [Xiaojun && Yangliu] add test "should_store_in_second_locker_and_return_ticket_when_store_bag_given_locker_capacity_are_4_and_5_respectively"

--- src/test/java/cn/xpbootcamp/locker_robot/SmartRobotTest.java
+++ src/test/java/cn/xpbootcamp/locker_robot/SmartRobotTest.java
@@ -23,4 +23,20 @@ public class SmartRobotTest {
         Assertions.assertNotNull(ticket);
         Assertions.assertEquals(locker1,smartRobot.ticketLockerMap.get(ticket));
     }
+
+    @Test
+    void should_store_in_second_locker_and_return_ticket_when_store_bag_given_locker_capacity_are_4_and_5_respectively() throws NoAvailableLockerBoxException {
+        ArrayList<Locker> lockers = new ArrayList<>();
+        Locker locker1 = new Locker(4);
+        Locker locker2 = new Locker(5);
+        lockers.add(locker1);
+        lockers.add(locker2);
+        SmartRobot smartRobot = new SmartRobot(lockers);
+
+        Bag bag = new Bag();
+        Ticket ticket = smartRobot.store(bag);
+
+        Assertions.assertNotNull(ticket);
+        Assertions.assertEquals(locker2,smartRobot.ticketLockerMap.get(ticket));
+    }
 }
